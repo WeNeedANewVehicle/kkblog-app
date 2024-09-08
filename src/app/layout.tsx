bd571b5aeb699fefcac71a6b63558e7339c6acd3
HandsomeChoco <kkb931115@gmail.com>
2024-09-08 19:40:12 +0900

    기아 폰트 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { kia } from "@/theme/font";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "KKBlog - 크크블로그",
@@ -14,9 +12,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={kia.className}>{children}</body>
     </html>
   );
 }
