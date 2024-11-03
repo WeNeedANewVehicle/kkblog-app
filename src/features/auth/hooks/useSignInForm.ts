176c6468380b922538fd15bd15be2987332259a6
HandsomeChoco <kkb931115@gmail.com>
2024-11-04 00:51:52 +0900

    훅 리팩토링

--- src/features/auth/hooks/useSignInForm.ts
+++ src/features/auth/hooks/useSignInForm.ts
@@ -1,4 +1,4 @@
-import React, { FormEvent, useCallback } from 'react'
+import React, { FormEvent, useCallback, useMemo } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import useSignIn from './queries/useSignIn';
@@ -10,26 +10,27 @@ const defaultValues = {
 };
 
 function useSignInForm() {
-    const { mutate: signIn, } = useSignIn();
+
     const { control, register, getValues, handleSubmit, formState } = useForm<SignInSchema>({
         resolver: zodResolver(signInSchema),
         defaultValues,
     });
 
+    const { mutate: signIn, isPending: isSignInPending } = useSignIn();
+
     const { errors } = formState;
 
-    const onSubmit = useCallback(async (values: SignInSchema) => {
-        signIn({
-            email: values.email,
-            password: values.password
-        })
+    const onSubmit = useCallback(async ({ email, password }: SignInSchema) => {
+        signIn({ email, password })
     }, [signIn]);
+
+    const isPending = useMemo(() => isSignInPending, [isSignInPending])
 
     return {
         register,
-        onSubmit,
-        handleSubmit,
+        onSubmit: handleSubmit(onSubmit),
         errors,
+        isPending
     }
 }
 
