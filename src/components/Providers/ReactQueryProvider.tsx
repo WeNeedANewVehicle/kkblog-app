f36e6dee3228e024b49a7d0fe7d04e2f4bce7c4d
HandsomeChoco <kkb931115@gmail.com>
2025-04-29 15:08:54 +0900

    fix: mutation 에도 retry 를 적용 및 공통 함수로 뺴기

--- src/components/Providers/ReactQueryProvider.tsx
+++ src/components/Providers/ReactQueryProvider.tsx
@@ -1,29 +1,35 @@
 'use client'
+import { ErrorBaseResponse } from '@/common/dto/baseResponse'
 import { HttpStatus } from '@/common/enum/http-status.enum'
 import { isServer, QueryClient } from '@tanstack/react-query'
+
+function retry(failureCount: number, error: ErrorBaseResponse) {
+  switch (error.meta.status) {
+    case HttpStatus.NOT_FOUND:
+      return false
+    case HttpStatus.FORBIDDEN:
+      return false
+    default:
+      break;
+  }
+
+  if (error.meta.status === HttpStatus.UNAUTHORIZED && error.meta.code === 'ACCESS TOKEN EXPIRED') {
+    return true;  
+  }
+
+  return failureCount <= 5
+};
 
 function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
+      mutations: { retry },
       queries: {
         // SSR 에선 데이터를 가져온 뒤 일정 시간이 지났을 때 신선하지 않다고 판단할 기본 값을 0 이상으로 설정한다.
         // 클라이언트가 데이터를 다시 가져오는 작업을 피하기 위해서이다.
         staleTime: 60 * 1000,
         retryDelay: 500,
-        retry: (failureCount, error) => {
-          switch (error.meta.status) {
-            case HttpStatus.NOT_FOUND:
-              return false
-            case HttpStatus.FORBIDDEN:
-              return false
-          }
-
-          if (error.meta.status === HttpStatus.UNAUTHORIZED && error.meta.code === 'ACCESS TOKEN EXPIRED') {
-            return true;  
-          }
-
-          return failureCount <= 5
-        },
+        retry
       },
     },
   })
