3e1e001baa6a27575c8f11bcde9f1af82fdbecf1
HandsomeChoco <kkb931115@gmail.com>
2025-04-29 23:50:43 +0900

    feat: confirm modal 변경

--- src/components/Modal/ConfirmModal/ConfirmModal.tsx
+++ src/components/Modal/ConfirmModal/ConfirmModal.tsx
@@ -1,36 +1,73 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Modal, { ModalProps } from '@/components/Modal/Modal'
-import style from '@/components/Modal/ConfirmModal/ConfirmModal.module.css'
-import { UseFormReturn } from 'react-hook-form'
-import { PostSchema } from '@/features/posts/schema/post.schema'
 import CloseIcon from '@/../public/icons/close.svg'
+import Button from '@/components/Button/Button'
 
 interface ConfirmModalProps extends ModalProps {
   onClose: () => void
-  register: UseFormReturn<PostSchema>['register']
   onConfirm: () => void
-  onUploadThumbnail: () => void
   isOpen: boolean
+  cancelText?: string
+  confirmText?: string
+  isPending?: boolean
 }
 
 function ConfirmModal({
   title,
   isOpen,
+  isPending,
   onClose,
   children,
+  onConfirm,
+  cancelText = '취소',
+  confirmText = '확인',
   ...rest
 }: ConfirmModalProps) {
+  if (!isOpen) {
+    return
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', (e) => {
+      e.preventDefault()
+    })
+  }, [])
+
   return (
-    <Modal
-      className={`flex flex-column flex-1 gap-1 ${style.wrapper} ${!isOpen && style.closed}`}
-      isOpen
-      {...rest}
-    >
-      <CloseIcon className={`flex self-end ${style.close}`} onClick={onClose} />
-      <h2 className={`flex self-center font-bold ${style.title}`}>{title}</h2>
-      {children}
-    </Modal>
+    <>
+      <div className="overlay overflow-hidden" />
+      <Modal
+        className={`screen-center modal z-10000 ${isOpen ? 'fixed' : 'hidden'} dark:text-white`}
+        isOpen
+        {...rest}
+      >
+        <h2 className={`flex justify-between items-center font-bold`}>
+          {title}
+          <Button className={'flex self-end icon-btn'} onClick={onClose}>
+            <CloseIcon className="icon interact-gray-icon dark:stroke-white!" />
+          </Button>
+        </h2>
+
+        {children}
+        <div className="flex gap-2 justify-end">
+          <Button
+            className="btn-black box-sm-wide"
+            onClick={onClose}
+            isLoading={isPending}
+          >
+            {cancelText}
+          </Button>
+          <Button
+            className="btn-warning box-sm-wide"
+            onClick={onConfirm}
+            isLoading={isPending}
+          >
+            {confirmText}
+          </Button>
+        </div>
+      </Modal>
+    </>
   )
 }
 
-export default ConfirmModal
+export default React.memo(ConfirmModal)
