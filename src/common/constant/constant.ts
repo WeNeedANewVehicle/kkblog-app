fddae8933d73ff2ca35c4dfb26a2c9b9f57194fc
HandsomeChoco <kkb931115@gmail.com>
2025-05-17 17:53:56 +0900

    fix: server url change

--- src/common/constant/constant.ts
+++ src/common/constant/constant.ts
@@ -2,7 +2,6 @@ import route from '@/routes/routes'
 import { OrderBy } from '../types/orderBy.type'
 
 export const isProd: boolean = process.env.NODE_ENV === 'production'
-
 export const baseUrl = isProd
   ? 'https://api.kkblog.co.kr'
   : 'http://localhost:8080'
