f43138d1a29dda6297b627434fe284ae83b494d9
HandsomeChoco <kkb931115@gmail.com>
2025-05-17 17:53:53 +0900

    fix: server url

--- src/common/constant/constant.ts
+++ src/common/constant/constant.ts
@@ -4,7 +4,7 @@ import { OrderBy } from '../types/orderBy.type'
 export const isProd: boolean = process.env.NODE_ENV === 'production'
 
 export const baseUrl = isProd
-  ? 'https://api.kkblog.p-e.kr'
+  ? 'https://api.kkblog.co.kr'
   : 'http://localhost:8080'
 
 export enum METHODS {
