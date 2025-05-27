0e1442205de135a352fdd7020d15b11aa3e76286
HandsomeChoco <kkb931115@gmail.com>
2025-05-28 03:07:50 +0900

    fix: 게시물 등록 후 게시물 목록이 갱신되도록 변경

--- src/features/posts/hooks/useCreatePost.ts
+++ src/features/posts/hooks/useCreatePost.ts
@@ -1,18 +1,25 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { createPostApi } from '@/features/posts/api/posts'
 import route from '@/routes/routes'
 import { useRouter } from 'next/navigation'
+import { GET_INFINITE_POSTS } from './useInfiniteGetPosts'
 
 const CREATE_POST = 'CREATE_POST'
 
 function useCreatePost() {
   const router = useRouter()
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationKey: [CREATE_POST],
     mutationFn: createPostApi,
     onSuccess: (res) => {
+      // 글 등록 후 글 목록을 갱신
+      queryClient.invalidateQueries({
+        queryKey: [GET_INFINITE_POSTS]
+      })
       router.replace(route.posts.detail(res.data.id))
+
     },
   })
 }
