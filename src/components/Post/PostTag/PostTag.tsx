34696de53807f2a3d7cab4342c6ba907287a0195
HandsomeChoco <kkb931115@gmail.com>
2025-05-28 03:04:30 +0900

    feat: 태그 클릭시 해당 태그가 포함된 게시물이 검색되도록 변경

--- src/components/Post/PostTag/PostTag.tsx
+++ src/components/Post/PostTag/PostTag.tsx
@@ -1,7 +1,10 @@
+'use client'
 import React, { MouseEventHandler } from 'react'
 import CloseIcon from '@/../public/icons/close.svg'
 import Button from '@/components/Button/Button'
 import { Tag } from '@/features/tags/types/tag.type'
+import Link from 'next/link'
+import route from '@/routes/routes'
 
 export type PostTagData = Pick<Tag, 'label' | 'id'>
 export interface PostTagProps extends PostTagData {
@@ -11,16 +14,23 @@ export interface PostTagProps extends PostTagData {
 
 function PostTag({ label, onDelete, isEdit }: PostTagProps) {
   return (
-    <li className="flex items-center bg-black gap-half text-white py-1 px-4 text-[0.8rem] whitespace-nowrap rounded-4xl h-8 gap-1">
-      {label}
-      {isEdit && (
-        <Button
-          className="bg-transparent hover:bg-gray-600 hover:rounded-4xl"
-          onClick={onDelete}
-        >
-          <CloseIcon className="w-4 h-4 cursor-pointer stroke-2 [&>path]:stroke-white" />
-        </Button>
-      )}
+    <li>
+      <Link
+        className={`flex items-center bg-black  text-white py-1 px-4 text-[0.8rem] whitespace-nowrap rounded-4xl h-8 gap-1 ${isEdit && 'cursor-text'}`}
+        href={`${route.posts.index}?search=${label}`}
+        onClick={(e) => isEdit && e.preventDefault()}
+      >
+        {label}
+        {isEdit && (
+          <Button
+            className="bg-transparent hover:bg-gray-600 hover:rounded-4xl"
+            onClick={onDelete}
+          >
+            <CloseIcon className="w-4 h-4 cursor-pointer stroke-2 [&>path]:stroke-white" />
+          </Button>
+        )}
+      </Link>
+      
     </li>
   )
 }
