5ad6e8ae18cfc87413f6409c1716a17347c3e526
HandsomeChoco <kkb931115@gmail.com>
2025-06-15 00:41:44 +0900

    fix: 로그인 시 사용자 정보를 즉시 갱신

--- src/features/auth/hooks/queries/useSignIn.ts
+++ src/features/auth/hooks/queries/useSignIn.ts
@@ -1,19 +1,26 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { signInApi } from '@/features/auth/api/auth'
 import { useRouter } from 'next/navigation'
 import redirectStorage from '@/common/storages/redirect-storage'
 import route from '@/routes/routes'
+import { ME } from '@/features/auth/hooks/queries/useMe'
 
 export const SIGN_IN = 'SIGN_IN'
 
 function useSignIn() {
   const router = useRouter()
   const redirectionUrl = redirectStorage.getRedirectUrl()
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: signInApi,
     retry: false,
     onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [ME]
+      });
+
       router.replace(redirectionUrl ?? route.index)
+
     },
   })
 }
